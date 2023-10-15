bf3119685fc96c7a0694892c7a63d752b6fbd6ff
--global <--global>
2023-10-16 04:41:50 +0700

    update env

--- backend/config/configuration.ts
+++ backend/config/configuration.ts
@@ -1,5 +1,5 @@
 export default () => ({
-  PORT: parseInt(process.env.PORT, 10) || 3000,
+  PORT: parseInt(process.env.PORT, 10) || 8080,
   GITHUB_TOKEN: process.env.GITHUB_TOKEN,
   MONGODB_URI: process.env.MONGODB_URI,
 });
--- backend/dist/config/configuration.js
+++ backend/dist/config/configuration.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = () => ({
-    PORT: parseInt(process.env.PORT, 10) || 3000,
+    PORT: parseInt(process.env.PORT, 10) || 8080,
     GITHUB_TOKEN: process.env.GITHUB_TOKEN,
     MONGODB_URI: process.env.MONGODB_URI,
 });
